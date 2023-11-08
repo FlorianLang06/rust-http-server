a498c67b7f6d60afd540b3cf3d1e5b84e62c67f8
Florian Lang <144521337+FlorianLang06@users.noreply.github.com>
2023-11-08 08:55:28 +0000

    feat: rewrite directory to index file

--- src/file.rs
+++ src/file.rs
@@ -1,6 +1,13 @@
 use std::fs;
 
 pub fn load_file(path: String) -> Result<String, String> {
+
+    let mut path = path;
+    
+    if path.ends_with("/") {//load index.html if path is a directory
+        path = path + "index.html";
+    }
+
     let file_content = match fs::read_to_string(path) {
         Ok(c) => c,
         Err(err) => return Err(err.to_string()),
